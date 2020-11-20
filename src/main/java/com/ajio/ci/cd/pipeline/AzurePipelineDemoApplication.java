0a96489a4de4ce257ee2c9de65793cab6cb5f37d
Vibhu.Garg <Vibhu.Garg@ril.com>
2020-11-20 13:03:35 +0530

    Code commited

---
+++ src/main/java/com/ajio/ci/cd/pipeline/AzurePipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ajio.ci.cd.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurePipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurePipelineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajio/ci/cd/pipeline/AzurePipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajio.ci.cd.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurePipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
